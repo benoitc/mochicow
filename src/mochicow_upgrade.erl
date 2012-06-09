57f47c97b000f956865e2903fb46ad91a7a55d06
benoitc <bchesneau@gmail.com>
2012-06-09 23:19:29 +0200

    add missing header

--- src/mochicow_upgrade.erl
+++ src/mochicow_upgrade.erl
@@ -1,3 +1,9 @@
+%M% -*- tab-width: 4;erlang-indent-level: 4;indent-tabs-mode: nil -*-
+%%% ex: ft=erlang ts=4 sw=4 et
+%%%
+%%% This file is part of mochicow released under the MIT license.
+%%% See the NOTICE for more information.
+%%%
 -module(mochicow_upgrade).
 
 -export([upgrade/4]).
