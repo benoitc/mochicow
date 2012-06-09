5d7f1c3276f6937069a5d20773ba13e37cbb885a
benoitc <bchesneau@gmail.com>
2012-06-09 16:01:43 +0200

    fix raw path & host
    In cowboy raw isn't raw and the raw path isn't the reauest path from the
    status line neither the raw host is the Host header. This patch fix
    this. Solve some issue I got with some requests.

--- src/mochicow_upgrade.erl
+++ src/mochicow_upgrade.erl
@@ -3,15 +3,14 @@
 -export([upgrade/4]).
 
 -include_lib("cowboy/include/http.hrl").
--include("mochicow.hrl").
-
-upgrade(_ListenerPid, _Handler, Opts, Req) ->
+-include("mochicow.hrl"upgrade(_ListenerPid, _Handler, Opts, Req) ->
     {loop, HttpLoop} = proplists:lookup(loop, Opts),
     #http_req{socket=Socket,
               transport=Transport,
               method=Method,
               version=Version,
               raw_path=Path,
+              raw_qs=QS,
               headers=Headers,
               raw_host=Host,
               port=Port,
@@ -19,17 +18,41 @@ upgrade(_ListenerPid, _Handler, Opts, Req) ->
 
     MochiSocket = mochiweb_socket(Transport, Socket),
     MochiHeaders = mochiweb_headers:make(Headers),
-    MochiHost = binary_to_list(Host) ++ ":" ++ integer_to_list(Port),
+    DefaultPort = default_port(Transport:name()),
+    MochiHost = case Port of
+        DefaultPort ->
+            Port;
+        _ ->
+            %% fix raw host
+            binary_to_list(Host) ++ ":" ++ integer_to_list(Port)
+    end,
     MochiHeaders1 = mochiweb_headers:enter('Host', MochiHost,
                 MochiHeaders),
 
+    %% fix raw path
+    Path1 = case Path of
+        <<>> ->
+            <<"/">>;
+        _ ->
+            Path
+    end,
+    RawPath = case QS of
+        <<>> ->
+            Path1;
+        _ ->
+            << Path1/binary, "?", QS/binary >>
+    end,
     MochiReq = mochiweb_request:new(MochiSocket,
                                     Method,
-                                    binary_to_list(Path),
+                                    binary_to_list(RawPath),
                                     Version,
                                     MochiHeaders1),
-
-    erlang:put(mochiweb_request_body, Buffer),
+    case Buffer of
+        <<>> -> ok;
+        _ ->
+            %%gen_tcp:unrecv(Socket, Buffer)
+            erlang:put(mochiweb_request_body, Buffer)
+    end,
     call_body(HttpLoop, MochiReq),
     after_response(Req, MochiReq).
 
@@ -47,17 +70,14 @@ call_body(Body, Req) ->
 
 after_response(Req, MochiReq) ->
     Connection =MochiReq:get_header_value("connection"),
-    Req2 = Req#http_req{connection=list_to_connection(Connection),
-                        resp_state=done},
+    Req2 = Req#http_req{connection = list_to_connection(Connection),
+                        resp_state = done,
+                        body_state = done,
+                        buffer = <<>> },
 
-    case MochiReq:should_close() of
-        true ->
-            close;
-        _ ->
-            MochiReq:cleanup(),
-            erlang:garbage_collect(),
-            {ok, Req2}
-    end.
+    MochiReq:cleanup(),
+    erlang:garbage_collect(),
+    {ok, Req2}.
 
 list_to_connection(Connection) when is_binary(Connection) ->
     list_to_connection(binary_to_list(Connection));
@@ -65,5 +85,9 @@ list_to_connection(Connection) when is_atom(Connection) ->
     Connection;
 list_to_connection("keep-alive") ->
     keepalive;
-list_to_connection("close") ->
+list_to_connection(_) ->
     close.
+
+-spec default_port(atom()) -> 80 | 443.
+default_port(ssl) -> 443;
+default_port(_) -> 80.
